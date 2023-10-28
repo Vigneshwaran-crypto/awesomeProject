97ef449c4f00f7339106555eee88884afe45d13b
manibharathyr <manibharathy@betamonks.com>
2023-10-28 16:48:44 +0530

    fireStore downloaded

--- src/app/Practice.js
+++ src/app/Practice.js
@@ -1,5 +1,12 @@
-import React, {useEffect} from 'react';
-import {View, Text, StyleSheet, Alert, TouchableOpacity} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Alert,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
 import {colors} from '../Common/colors';
 import messaging from '@react-native-firebase/messaging';
 import {
@@ -9,9 +16,15 @@ import {
   onDisplayNotification,
   requestUserPermission,
 } from '../Common/utils';
-import {textFontFaceSemiBold} from '../Common/styles';
+import {textFontFaceMedium, textFontFaceSemiBold} from '../Common/styles';
+import {firebase} from '@react-native-firebase/firestore';
 
 const Practice = () => {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [contact, setContact] = useState('');
+  const [email, setEmail] = useState('');
+
   //proper working modal for push notification
   // useEffect(() => {
   //   const unsubscribe = messaging().onMessage(async remoteMessage => {
@@ -29,16 +42,47 @@ const Practice = () => {
   //   const token = getToken();
   // }, []);
 
-  // const onDisPress = () => {
-  //   onDisplayNotification();
-  // };
+  const onSavePress = () => {};
 
   return (
     <View style={styles.container}>
-      <Text>Hello everyone </Text>
+      <View style={styles.detailContainer}>
+        <Text style={styles.titleText}>User Details</Text>
 
-      <TouchableOpacity style={styles.displayNotifyBut} onPress={onDisPress}>
-        <Text style={styles.buttonText}>Display Notify</Text>
+        <Text style={styles.labelText}>Name :</Text>
+        <TextInput
+          style={styles.inputField}
+          onChangeText={setName}
+          value={name}
+        />
+
+        <Text style={styles.labelText}>Age :</Text>
+        <TextInput
+          style={styles.inputField}
+          keyboardType="decimal-pad"
+          onChangeText={setAge}
+          value={age}
+        />
+
+        <Text style={styles.labelText}>Contact :</Text>
+        <TextInput
+          style={styles.inputField}
+          keyboardType="decimal-pad"
+          onChangeText={setContact}
+          value={contact}
+        />
+
+        <Text style={styles.labelText}>Email :</Text>
+        <TextInput
+          style={styles.inputField}
+          keyboardType="email-address"
+          onChangeText={setEmail}
+          value={email}
+        />
+      </View>
+
+      <TouchableOpacity style={styles.saveButton} onPress={onSavePress}>
+        <Text style={styles.saveText}>Save</Text>
       </TouchableOpacity>
     </View>
   );
@@ -48,16 +92,49 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
   },
-  displayNotifyBut: {
-    padding: 10,
-    backgroundColor: colors.borderGrey,
+  detailContainer: {
+    height: '60%',
+    width: '80%',
+    // borderWidth: 1,
+    elevation: 5,
+    zIndex: 5,
+    shadowColor: colors.activeGreen,
+    padding: 20,
+    // justifyContent: 'space-around',
+  },
+  titleText: {
+    fontFamily: textFontFaceMedium,
+    color: colors.darkGreen,
     alignSelf: 'center',
-    borderRadius: 10,
+    fontSize: 25,
   },
-  buttonText: {
-    color: colors.activeGreen,
-    fontFamily: textFontFaceSemiBold,
+  labelText: {
+    fontFamily: textFontFaceMedium,
+    color: colors.black,
+    fontWeight: '900',
+    fontSize: 20,
+    marginTop: 15,
+  },
+
+  inputField: {
+    borderWidth: 1,
+    marginHorizontal: 20,
+    paddingVertical: 8,
+    marginTop: 10,
+  },
+  saveButton: {
+    backgroundColor: colors.activeGreen,
+    borderRadius: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginTop: 20,
+  },
+  saveText: {
+    fontWeight: '900',
+    color: colors.white,
+    alignSelf: 'center',
   },
 });
 
